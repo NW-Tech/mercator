ed4cc1413791ed57de6c81f4f28d1a965500cedd
dbarzin <dbarzin@gmail.com>
2026-05-09 10:10:53 +0200

    add missing views

---
+++ resources/views/admin/application-flows/show.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.admin')
+
+@section('title')
+    {{ $flow->name }}
+@endsection
+
+@section('content')
+    <div class="form-group">
+        <a class="btn btn-default" href="{{ route('admin.application-flows.index') }}">
+            {{ trans('global.back_to_list') }}
+        </a>
+
+        <a class="btn btn-success"
+           href="{{ route('admin.report.explore') }}?node={{$flow->sourceId()}},{{$flow->destId()}}">
+            {{ trans('global.explore') }}
+        </a>
+
+        @can('flux_edit')
+            <a class="btn btn-info" href="{{ route('admin.application-flows.edit', $flow->id) }}">
+                {{ trans('global.edit') }}
+            </a>
+        @endcan
+
+        @can('flux_delete')
+            <form action="{{ route('admin.application-flows.destroy', $flow->id) }}" method="POST"
+                  onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
+                <input type="hidden" name="_method" value="DELETE">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <input type="submit" class="btn btn-danger" value="{{ trans('global.delete') }}">
+            </form>
+        @endcan
+    </div>
+    <div class="card">
+        <div class="card-header">
+            {{ trans('global.show') }} {{ trans('cruds.flux.title') }}
+        </div>
+        <div class="card-body">
+            @include('admin.application-flows._details', [
+                'flux' => $flow,
+                'withLink' => false,
+            ])
+        </div>
+        <div class="card-footer">
+            {{ trans('global.created_at') }} {{ $flow->created_at ? $flow->created_at->format(trans('global.timestamp')) : '' }}
+            |
+            {{ trans('global.updated_at') }} {{ $flow->updated_at ? $flow->updated_at->format(trans('global.timestamp')) : '' }}
+        </div>
+    </div>
+    <div class="form-group">
+        <a id="btn-cancel" class="btn btn-default" href="{{ route('admin.application-flows.index') }}">
+            {{ trans('global.back_to_list') }}
+        </a>
+    </div>
+@endsection
