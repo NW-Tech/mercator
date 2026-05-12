0f1dafd47f8e161d27ace2489fd96c1a69807e3e
dbarzin <dbarzin@gmail.com>
2026-05-12 11:41:12 +0200

    fix invalid key

--- app/Http/Controllers/Report/AuditController.php
+++ app/Http/Controllers/Report/AuditController.php
@@ -314,13 +314,13 @@ class AuditController extends HomeController
 
         // flux
         $sheet->setCellValue("A{$row}", trans('cruds.flux.title'));
-        $sheet->setCellValue("B{$row}", $levels['fluxes_lvl1']);
+        $sheet->setCellValue("B{$row}", $levels['flows_lvl1']);
         $sheet->setCellValue("C{$row}", $levels['fluxes']);
         $sheet->setCellValue("D{$row}", "=B{$row}/C{$row}");
-        $sheet->setCellValue("E{$row}", $levels['fluxes_lvl1']);
+        $sheet->setCellValue("E{$row}", $levels['flows_lvl1']);
         $sheet->setCellValue("F{$row}", $levels['fluxes']);
         $sheet->setCellValue("G{$row}", "=E{$row}/F{$row}");
-        $sheet->setCellValue("H{$row}", $levels['fluxes_lvl1']);
+        $sheet->setCellValue("H{$row}", $levels['flows_lvl1']);
         $sheet->setCellValue("I{$row}", $levels['fluxes']);
         $sheet->setCellValue("J{$row}", "=H{$row}/I{$row}");
         $row++;
